5a49677d449759c086c990bb3cc33c137eb7916b
aryan007 patel <aryan007.lko@gmail.com>
2025-09-16 16:36:20 +0530

    add link in cors to run frontend

--- Backend/Server.js
+++ Backend/Server.js
@@ -14,7 +14,7 @@ const PORT = 8080;
 
 // CORS configuration
 app.use(cors({
-    origin: ["http://localhost:5173", "http://localhost:5174", "https://blink-frontend.onrender.com", "https://blink-chatbot-three.vercel.app"],
+    origin: ["http://localhost:5173", "http://localhost:5174", "https://blink-frontend.onrender.com", "https://blink-chatbot-1.onrender.com"],
     credentials: true
 }));
 
